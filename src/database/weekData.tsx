7eb28b6f49a5048a2721c8d7cec1010f33152662
maeng99 <137726145+maeng99@users.noreply.github.com>
2024-08-07 00:11:49 +0900

    feat: 데이터 추가
    데이터 추가

--- src/database/weekData.tsx
+++ src/database/weekData.tsx
@@ -5,6 +5,19 @@ import '../App.css';
 export default function WeekData() {
     return [
         {
+            start_date: '2024-07-29',
+            end_date: '2024-08-04',
+            week_day: [
+                { name: '월', Life: 5, Work: 5, Exercise: 30, Sleep: 6 }, // Add Exercise data
+                { name: '화', Life: 3.5, Work: 6.5, Exercise: 30, Sleep: 7 },
+                { name: '수', Life: 4, Work: 6, Exercise: 30, Sleep: 8 },
+                { name: '목', Life: 5, Work: 5, Exercise: 30, Sleep: 8 },
+                { name: '금', Life: 7, Work: 3, Exercise: 60, Sleep: 3 },
+                { name: '토', Life: 7, Work: 3, Exercise: 70, Sleep: 8 },
+                { name: '일', Life: 9, Work: 1, Exercise: 110, Sleep: 9 },
+            ],
+        },
+        {
             start_date: '2024-07-22',
             end_date: '2024-07-28',
             week_day: [
